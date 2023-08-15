2cce68e87efba08253e5791a913bc800721c8d9b
Alejandro Jacobo <alecaperez13@gmail.com>
2023-08-15 11:33:09 -0500

    agregamos el rezise del canvas

--- game.js
+++ game.js
@@ -1,8 +1,34 @@
+console.log(maps);
+
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame);
+let canvaSize;
+let elementSize;
+
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvaSize = window.innerWidth * 0.8;
+  } else {
+    canvaSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvaSize);
+  canvas.setAttribute("height", canvaSize);
+
+  elementSize = canvaSize / 10 - 1;
+
+  startGame();
+}
 
 function startGame() {
-  game.fillRect(0, 0, 100, 100);
+  game.font = elementSize + "px Verdana";
+  game.textAlign = "";
+
+  for (let i = 0; i < +10; i++) {
+    game.fillText(emojis["X"], elementSize * i, elementSize);
+  }
 }
