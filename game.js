04429c41ae9f46b4dabaada3af6558bee2a651db
Alejandro Jacobo <alecaperez13@gmail.com>
2023-09-18 12:52:28 -0500

    Agreamos el player dentro del canvas

--- game.js
+++ game.js
@@ -2,9 +2,18 @@ console.log(maps);
 
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvaSize;
 let elementSize;
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -19,7 +28,7 @@ function setCanvasSize() {
   canvas.setAttribute("width", canvaSize);
   canvas.setAttribute("height", canvaSize);
 
-  elementSize = canvaSize / 10 - 1;
+  elementSize = canvaSize / 10;
 
   startGame();
 }
@@ -28,9 +37,51 @@ function startGame() {
   game.font = elementSize + "px Verdana";
   game.textAlign = "";
 
-  for (let row = 0; row < 10; row++) {
-    for (let col = 0; col < 10; col++) {
-      game.fillText(emojis["X"], elementSize * col, elementSize * row);
-    }
-  }
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowCols = mapRows.map((row) => row.trim().split(""));
+
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementSize * colI;
+      const posY = elementSize * (rowI + 1);
+
+      if (col == "O") {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+        console.log({ playerPosition });
+      }
+
+      game.fillText(emoji, posX, posY);
+    });
+  });
+
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(e) {
+  if (e.key == "ArrowUp") moveUp();
+  else if (e.key == "ArrowLeft") moveLeft();
+  else if (e.key == "ArrowRight") moveRight();
+  else if (e.key == "ArrowDown") moveDown();
+}
+
+function moveUp() {
+  console.log("Me movi hacia arriba");
+}
+function moveLeft() {
+  console.log("Me movi hacia la izquierda");
+}
+function moveRight() {
+  console.log("Me movi hacia la derecha");
+}
+function moveDown() {
+  console.log("Me movi hacia abajo");
 }
